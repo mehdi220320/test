983b3c8a244e76d1df3001903e7455f2e20af7e7
Mohamed Mehdi Ben Gamra <111698017+mehdi220320@users.noreply.github.com>
2025-09-19 10:14:23 +0100

    Update web_registry.dart

--- lib/web_registry.dart
+++ lib/web_registry.dart
@@ -1,10 +1,10 @@
-// web_registry.dart
-// Only for web
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:ui' as ui;
-import 'dart:html' as html;
+// // web_registry.dart
+// // Only for web
+// // ignore: avoid_web_libraries_in_flutter
+// import 'dart:ui' as ui;
+// import 'dart:html' as html;
 
-void registerVideoElement(html.VideoElement videoElement, String viewType) {
-  // ignore: undefined_prefixed_name
-  ui.platformViewRegistry.registerViewFactory(viewType, (int viewId) => videoElement);
-}
+// void registerVideoElement(html.VideoElement videoElement, String viewType) {
+//   // ignore: undefined_prefixed_name
+//   ui.platformViewRegistry.registerViewFactory(viewType, (int viewId) => videoElement);
+// }
